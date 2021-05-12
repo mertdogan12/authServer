0d6ff98a8834ec75e262f15f80490221f9b44114
Mert Dogan <42122932+supermert9@users.noreply.github.com>
2021-05-12 21:13:54 +0200

    Jwt removed, MongoDb String update

--- Startup.cs
+++ Startup.cs
@@ -23,7 +23,7 @@ namespace authServer
         public IConfiguration Configuration { get; }
         public static string databaseName = "";
 
-        // This method gets called by the runtime. Use this method to add services to the container.
+        // This methrunod gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
 
@@ -31,18 +31,6 @@ namespace authServer
 
             BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
             BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-
-            services.AddAuthentication(options =>
-                             {
-                                 options.DefaultAuthenticateScheme = JwtAuthenticationDefaults.AuthenticationScheme;
-                                 options.DefaultChallengeScheme = JwtAuthenticationDefaults.AuthenticationScheme;
-                             })
-                        .AddJwt(options =>
-                             {
-                                 options.Keys = new[] { "GQDstcKsx0NHjPOuXOYg5MbeJ1XT0uFiwDVvVBrk" };
-
-                                 options.VerifySignature = true;
-                             });
 
             services.AddSingleton<IMongoClient>(ServiceProvider =>
             {
