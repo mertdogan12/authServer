2822e9e46a013963a53296fb97a13f6ce1128d84
Mert Dogan <mert.dogan@me.com>
2021-07-01 20:55:25 +0200

    Env vars addded

--- Settings/MongoDbSettings.cs
+++ Settings/MongoDbSettings.cs
@@ -1,13 +1,64 @@
+using System;
+
 namespace authServer.Settings
 {
     public class MongoDbSettings
     {
-        public string Host { get; set; }
-        public int Port { get; set; }
-        public string Database { get; set; }
-        public string User { get; set; }
-        public string Password { get; set; }
-        public string Url { get; set; }
+        public string Host
+        {
+            get
+            {
+                return Environment.GetEnvironmentVariable("ASHOST");
+            }
+        }
+
+        public int Port
+        {
+            get
+            {
+                try
+                {
+                    return Int32.Parse(Environment.GetEnvironmentVariable("ASPORT"));
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine($"Error while parsing ASPORT env var (Port set to 27017) Error : {e}");
+                    return 27017;
+                }
+            }
+        }
+
+        public string Database
+        {
+            get
+            {
+                return Environment.GetEnvironmentVariable("ASDATABASE");
+            }
+        }
+
+        public string User
+        {
+            get
+            {
+                return Environment.GetEnvironmentVariable("ASUSER");
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return Environment.GetEnvironmentVariable("ASPW");
+            }
+        }
+
+        public string Url
+        {
+            get
+            {
+                return Environment.GetEnvironmentVariable("ASURL");
+            }
+        }
 
         public string connectionString
         {
